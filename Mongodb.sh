c17f9206ebd78d4287c301b17c8abb37d47ae950
unknown <kalyaninalamaru.com>
2023-12-27 14:07:47 +0530

    Roboshop-shell-scripts

--- Mongodb.sh
+++ Mongodb.sh
@@ -2,12 +2,12 @@
 
 ID=$(id-u)
 
-R= "\e [31m"
-G= "\e [32m"
-Y= "\e [33m"
-N= "\e [0m"
+R= "\e[31m"
+G= "\e[32m"
+Y= "\e[33m"
+N= "\e[0m"
 
-TIMESTAMP= $(date +%F -%H -%M -%S )
+TIMESTAMP= $(date +%F-%H-%M-%S)
 LOGFILE= "/tmp/$0-$TIMESTAMP.log"
 echo "script started exceuting at $TIMESTAMP" &>> $LOGFILE
 
